05495a2bd996384d3b18554b7f0d467d0672cd5c
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-08 20:03:10 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,7 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+temp = doc.css(".headline-260IBN")
+
+temp.each {|t| puts t.text.strip} 
