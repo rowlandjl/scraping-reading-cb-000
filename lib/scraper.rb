9a68590e6617de7f872b57ce96ef666b14cf7f9b
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-12 13:44:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,8 @@ html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 # puts doc 
 # binding.pry 
-temp = doc.css(".headline-26OIBN")
-puts temp 
+# temp = doc.css(".headline-26OIBN")
+# puts temp 
+temp = doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrif3-2XAK6G")
 
 temp.each {|t| puts t.text.strip} 
