4f1126a22b354efb2883cc0ae1b2c36fcecc5fb7
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-12 13:01:55 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,11 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
+puts doc 
+# binding.pry 
 temp = doc.css(".headline-260IBN")
 
 temp.each {|t| puts t.text.strip} 
